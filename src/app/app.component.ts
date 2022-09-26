c42f21e40f9256b7bc69cb308d3601e6baded98e
jpfuentesv89 <jua.fuentes@duocuc.cl>
2022-09-26 20:00:17 -0300

    detalles2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,20 +13,21 @@ export class AppComponent {
   public appPages = [
     { title: 'Inicio', url: '/folder/Inicio', icon: 'home' },
     { title: 'Casos', url: '/folder/Casos', icon: 'heart' },
-    { title: 'Quiénes somos', url: '/folder/Quiénes somos', icon: 'book' },
-    { title: 'Nuestro equipo', url: '/folder/Nuestro equipo', icon: 'people' },
     { title: 'Galería', url: '/folder/Galería', icon: 'image' },
-    { title: 'Contáctanos', url: '/folder/Contáctanos', icon: 'mail' },
   ];
   public labels = [
     { label: 'Donaciones', url: '/folder/Donaciones', icon: 'wallet' },
     { label: 'Apadrinamiento', url: '/folder/Apadrinamiento', icon: 'paw' },
-    { label: 'Unetenos', url: '/folder/Unetenos', icon: 'man' },
   ];
   public labels2 = [
     { label: 'Login', url: '/auth/login', icon: 'finger-print' },
     { label: 'Ajustes', url: '/admin/config', icon: 'cog' },
     { label: 'Logout', url: '', icon: 'exit' },
+  ];
+  public labels3 = [
+    { label: 'Quiénes somos', url: '/folder/Quiénes somos', icon: 'book' },
+    { label: 'Nuestro equipo', url: '/folder/Nuestro equipo', icon: 'people' },
+    { label: 'Contáctanos', url: '/folder/Contáctanos', icon: 'mail' },
   ];
   constructor(private auth: AuthenticationService, private router: Router) {
     auth.stateAuth().subscribe(res => {
