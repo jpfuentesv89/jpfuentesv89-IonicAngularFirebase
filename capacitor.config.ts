75e89a89f1c6e544cdb1b28030e3db9a3e7ef1cb
jpfuentesv89 <jua.fuentes@duocuc.cl>
2022-10-16 20:04:16 -0300

    G3-16/10/2022

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Bizcocho',
+  appName: 'veteriApps',
   webDir: 'www',
   bundledWebRuntime: false
 };
