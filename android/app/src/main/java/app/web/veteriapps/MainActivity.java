6c1a8a471993d3873bdc3b8effd28d3ee06193fa
jpfuentesv89 <jua.fuentes@duocuc.cl>
2022-10-18 22:27:51 -0300

    G11-18/10/2022

---
+++ android/app/src/main/java/app/web/veteriapps/MainActivity.java
@@ -0,0 +1,5 @@
+package app.web.veteriapps;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'app.web.veteriapps',
   appName: 'veteriApps',
   webDir: 'www',
   bundledWebRuntime: false
