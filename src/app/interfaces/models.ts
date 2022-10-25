71819ae928fefae624960b4fa7733b6e419f2fea
jpfuentesv89 <jua.fuentes@duocuc.cl>
2022-10-25 20:40:22 -0300

    G8-25/07/2022

--- src/app/interfaces/models.ts
+++ src/app/interfaces/models.ts
@@ -13,6 +13,81 @@ export interface Clientes {
     foto:      string;
 }
 
+export interface Administrador {
+    rut:       number;
+    dv:        string;
+    nombre:    string;
+    apaterno:  string;
+    amaterno:  string;
+    direccion: string;
+    comuna:    string;
+    email:     string;
+    telefono:  number;
+    username:  string;
+    uid:       string;
+    foto:      string;
+}
+
+export interface Peluquero {
+    rut:       number;
+    dv:        string;
+    nombre:    string;
+    apaterno:  string;
+    amaterno:  string;
+    direccion: string;
+    comuna:    string;
+    email:     string;
+    telefono:  number;
+    username:  string;
+    uid:       string;
+    foto:      string;
+}
+
+export interface Recepcionista {
+    rut:       number;
+    dv:        string;
+    nombre:    string;
+    apaterno:  string;
+    amaterno:  string;
+    direccion: string;
+    comuna:    string;
+    email:     string;
+    telefono:  number;
+    username:  string;
+    uid:       string;
+    foto:      string;
+}
+
+export interface Veterinario {
+    rut:       number;
+    dv:        string;
+    nombre:    string;
+    apaterno:  string;
+    amaterno:  string;
+    direccion: string;
+    comuna:    string;
+    email:     string;
+    telefono:  number;
+    username:  string;
+    uid:       string;
+    foto:      string;
+}
+
+export interface Bodegeros {
+    rut:       number;
+    dv:        string;
+    nombre:    string;
+    apaterno:  string;
+    amaterno:  string;
+    direccion: string;
+    comuna:    string;
+    email:     string;
+    telefono:  number;
+    username:  string;
+    uid:       string;
+    foto:      string;
+}
+
 export interface Productos {
     id:             string;
     nombre:         string;
