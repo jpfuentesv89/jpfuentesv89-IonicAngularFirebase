c5189bb2c4ae22edf0241ecc2e5124fa56c0eee9
DEMWOLFZX <ale.villa@duocuc.cl>
2022-10-30 23:09:31 -0300

    G2-30-10-2022

--- src/app/pages/tienda/cliente/cliente.page.ts
+++ src/app/pages/tienda/cliente/cliente.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/auth/services/authentication.service';
+import { FirestoreService } from 'src/app/services/firestore.service';
+import { InteractionService } from 'src/app/services/interaction.service';
 
 @Component({
   selector: 'app-cliente',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientePageTienda implements OnInit {
 
-  constructor() { }
+  productos: any;
+
+  constructor(private database: FirestoreService, private interaction: InteractionService, private auth: AuthenticationService) {
+
+    auth.stateAuth().subscribe(res => {      
+      if (res && res.uid) {
+
+      } else {
+        this.interaction.presentToast('usuario no logueado');
+        console.log('usuario no logueado');
+      }
+    });
+
+  }
 
   ngOnInit() {
+
+    const path = 'productos';
+    this.database.getdocs(path).subscribe(res => {
+      this.interaction.presentToast('Productos listados');
+      this.productos = res;
+    }, err => {
+      this.interaction.presentToast('Error al listar productos');
+      console.log(err);
+    });
+
   }
 
 }
