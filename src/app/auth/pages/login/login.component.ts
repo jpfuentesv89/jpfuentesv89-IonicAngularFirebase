8b5ad9f7720339eaf2b90dc953b3277164989a89
jpfuentesv89 <jua.fuentes@duocuc.cl>
2022-11-09 01:19:17 -0300

    G1-09/11/2022

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { InteractionService } from 'src/app/services/interaction.service';
 import { AuthenticationService } from '../../service/authentication.service';
+import { AvailableResult, NativeBiometric } from "capacitor-native-biometric";
 
 @Component({
   selector: 'app-login',
@@ -41,4 +42,26 @@ export class LoginComponent {
       this.interaction.presentToast('Ingrese un correo y contraseña válidos');
     }
   }
+
+  async authenticatebiometric() {
+    const isAvailable = await NativeBiometric.isAvailable();
+    if (isAvailable) {
+      const result = await NativeBiometric.verifyIdentity({
+        title: "Autenticación biométrica",
+        subtitle: "Logearse con huella",
+        description: "Por favor, coloque su huella digital",
+      });
+      const result2 = await this.auth.login('admin@admin.com', 'adminroot').catch(err => console.log(err));
+      if (!result && result2) {
+        console.log('inicio de sesión exitoso');
+        this.interaction.presentToast('Bienvenido ' + 'Administrador');
+        this.router.navigateByUrl('/pages/home');
+      }else {
+        this.interaction.presentToast('Autenticación Biométrica Desactivada');
+      }
+    } else {
+      this.interaction.presentToast('Biometric no disponible');
+    }
+  }
+
 }
