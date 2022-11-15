8df6d4e5ff50a90cfe112adc0d0f8246d91a244a
jpfuentesv89 <jua.fuentes@duocuc.cl>
2022-11-15 07:34:09 -0300

    G10-15/11/2022

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -32,6 +32,12 @@ export class LoginComponent {
   }
 
   ngOnInit() {
+    if (localStorage.getItem('firstReload') == 'true') {
+      localStorage.setItem('firstReload', 'false');
+      window.location.reload();
+    } else {
+      localStorage.setItem('firstReload', 'true');
+    }
   }
 
   async onSubmit() {
